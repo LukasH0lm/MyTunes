36382e2ab430b49cc9d66a08ccc00abb3c711df9
LukasH0lm <lukasholm@hotmail.com>
2022-11-30 13:25:23 +0100

    Made an absolutely astounding ui using the latest advances in graphic design, we have made objectively the best ui since Windows vista

--- src/main/java/com/monkeygang/MyTunes/Application/BuisnessLogic/MyTunesApplication.java
+++ src/main/java/com/monkeygang/MyTunes/Application/BuisnessLogic/MyTunesApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MyTunesApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/MyTunes-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MyTunesApplication.class.getResource("/MyTunes-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("πψτμηες");
         stage.setScene(scene);
         stage.show();
     }
