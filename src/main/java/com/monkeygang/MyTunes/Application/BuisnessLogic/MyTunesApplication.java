ec82092c4581651248513b2f76cef361b6730abb
sunwar01 <sunwar01@easv365.dk>
2022-12-01 09:54:46 +0100

    uhuhuhuhuhuhuuh

--- src/main/java/com/monkeygang/MyTunes/Application/BuisnessLogic/MyTunesApplication.java
+++ src/main/java/com/monkeygang/MyTunes/Application/BuisnessLogic/MyTunesApplication.java
@@ -3,8 +3,11 @@ package com.monkeygang.MyTunes.Application.BuisnessLogic;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MyTunesApplication extends Application {
@@ -15,6 +18,15 @@ public class MyTunesApplication extends Application {
         stage.setTitle("πψτμηες");
         stage.setScene(scene);
         stage.show();
+
+
+        //UUHUHUHUHHUH Eksempel på afspilning af lyd xD
+        File f = new File("src/main/resources/monke.mp3");
+        Media m = new Media(f.toURI().toString());
+        MediaPlayer mp = new MediaPlayer(m);
+        mp.play();
+
+
     }
 
     public static void main(String[] args) {
